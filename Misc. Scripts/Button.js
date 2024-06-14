7a0d70b4aeac350916edfeb7fd1e942e6300212a
Eli <future.software@outlook.com>
2024-06-14 14:59:29 -0700

    Created "Button.js" as a misc. script.

---
+++ Misc. Scripts/Button.js
@@ -0,0 +1,5 @@
+// This is an example script for a redirect button in a webste. The button woul have "onclick = 'Click()'" in it.
+
+function Click() {
+  window.location.replace("https://google.com") // Replace the URL with whatever you want
+}
